fffd8edc2ec3ae8357198e8d07ec6c6e476bdf26
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-05-05 14:08:27 -0300

    corregí texto agregré comillas en línea 21 de index

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,22 @@ let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
 
 let btnEnviar = document.getElementById("enviar");
-let rotulo1 = document.getElementById("rotulo1"); 
-let rotulo2 = document.getElementById("rotulo2"); 
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
 
-rotulo1.innerHTML=("Ingrese la cantidad de productos"); 
-rotulo2.innerHTML=("Ingrese el precio del producto"): 
+rotulo1.innerHTML = "Ingrese la cantidad de productos";
+rotulo2.innerHTML = "Ingrese el precio del producto";
 
 btnEnviar?.addEventListener("click", () => {
   let cantidad: number = Number(dato1.value);
   let precio: number = Number(dato2.value);
 
-  let compra : number = precio * cantidad;
+  let compra: number = precio * cantidad;
 
-if (compra > 1000){
-compra = compra * 0.90;
-console.log("Felicitaciones! Obtuviste un decuento : $", compra);
-
-}else {
-console.log(El total de su compra es : $ , compra);
-
-}
+  if (compra > 1000) {
+    compra = compra * 0.9;
+    console.log("Felicitaciones! Obtuviste un decuento : $ ", compra);
+  } else {
+    console.log("El total de su compra es : $", compra);
+  }
 });
